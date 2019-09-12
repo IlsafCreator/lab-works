1393c6a3e35e74466db76b9a6839c838fb3747c6
Ilsaf Khasanov <ilsaf.khasanov@mail.ru>
2019-09-12 08:31:40 +0300

    make search function

--- lab1/js/search.js
+++ lab1/js/search.js
@@ -1,1 +1,22 @@
-const search = document.getElementById('#search');
+//searching function
+function searching(){
+  //getting searching query from input field in lowercase 
+  searchQuery = document.getElementById('searchField').value.toLowerCase();
+
+  for (var i = 0; i < document.getElementsByClassName('content').length;i++){
+    //getting text content from div, removing the leading and trailing white space and line terminator characters and make text in lowercase
+    cardText = document.getElementsByClassName('content')[i].textContent.trim().toLowerCase();
+
+    if (searchQuery !== ''){
+      if (cardText.indexOf(searchQuery)!=-1){
+        document.getElementsByClassName('column')[i].style.display='block';
+      } else {
+        document.getElementsByClassName('column')[i].style.display='none';
+      }
+    }else{
+      document.getElementsByClassName('column')[i].style.display='block';
+    }
+  }
+}
+
+document.getElementById('searchButton').onclick=searching;
