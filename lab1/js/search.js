961849bf91b90b18b95ded73996ef71636d79d8b
Ilsaf Khasanov <ilsaf.khasanov@mail.ru>
2019-09-17 17:00:23 +0300

    fix logical expression

--- lab1/js/search.js
+++ lab1/js/search.js
@@ -1,16 +1,18 @@
 //search function
 function search() {
   //getting search query from input field in lowercase 
-  searchQuery = document.getElementById('searchField').value.toLowerCase();
+  var searchQuery = document.getElementById('searchField').value.toLowerCase();
 
   for (var i = 0; i < document.getElementsByClassName('content').length; i++) {
     //getting text content from div, removing the leading and trailing white space and line terminator characters and make text in lowercase
-    cardText = document.getElementsByClassName('content')[i].textContent.trim().toLowerCase();
+    var cardText = document.getElementsByClassName('content')[i].textContent.trim().toLowerCase();
 
     if (searchQuery !== '') {
       if (cardText.indexOf(searchQuery) <= -1) {
         document.getElementsByClassName('column')[i].style.display = 'none';
       }
+    }else{
+      document.getElementsByClassName('column')[i].style.display = 'block';
     }
 
     // if (searchQuery !== '') {
