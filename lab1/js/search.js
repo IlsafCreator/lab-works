41bc47ed9353936aeaca169f6c6bb1f7f5837c62
Ilsaf Khasanov <ilsaf.khasanov@mail.ru>
2019-09-17 16:29:49 +0300

    fix formatting

--- lab1/js/search.js
+++ lab1/js/search.js
@@ -1,28 +1,28 @@
 //searching function
-function searching(){
+function searching() {
   //getting searching query from input field in lowercase 
   searchQuery = document.getElementById('searchField').value.toLowerCase();
 
-  for (var i = 0; i < document.getElementsByClassName('content').length;i++){
+  for (var i = 0; i < document.getElementsByClassName('content').length; i++) {
     //getting text content from div, removing the leading and trailing white space and line terminator characters and make text in lowercase
     cardText = document.getElementsByClassName('content')[i].textContent.trim().toLowerCase();
 
-    if (searchQuery !== ''){
-      if (cardText.indexOf(searchQuery)!=-1){
-        document.getElementsByClassName('column')[i].style.display='block';
+    if (searchQuery !== '') {
+      if (cardText.indexOf(searchQuery) != -1) {
+        document.getElementsByClassName('column')[i].style.display = 'block';
       } else {
-        document.getElementsByClassName('column')[i].style.display='none';
+        document.getElementsByClassName('column')[i].style.display = 'none';
       }
-    }else{
-      document.getElementsByClassName('column')[i].style.display='block';
+    } else {
+      document.getElementsByClassName('column')[i].style.display = 'block';
     }
   }
 }
 
-document.querySelector('#searchField').addEventListener('keydown',function(event){
+document.querySelector('#searchField').addEventListener('keydown', function (event) {
   if (event.keyCode === 13) {
     searching();
   }
 })
 
-document.getElementById('searchButton').onclick=searching;
+document.getElementById('searchButton').onclick = searching;
