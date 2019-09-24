74bba850171ad6ef6f42c06230634a63cabc3008
Ilsaf Khasanov <ilsaf.khasanov@mail.ru>
2019-09-24 17:08:46 +0300

    delete stuff

--- lab2 (todo-app)/js/app.js
+++ lab2 (todo-app)/js/app.js
@@ -51,7 +51,7 @@ window.onload = function () {
       addTask();
     }
   })
-// complete all handler 
+  // complete all handler 
   var completeAll = document.getElementById('complete-all');
   completeAll.onclick = function () {
     for (let index = 0; index < document.getElementsByClassName('new checkbox').length; index++) {
@@ -79,45 +79,3 @@ window.onload = function () {
     };
   */
 }
-//stuff
-// $(document).ready(function () {
-//   var btn = $('#button');
-//   // btn.prop("disabled", true);
-//   var textarea = $('#textarea');
-
-
-
-//   btn.click(function () {
-//     addTask();
-//     textarea.val('');
-//   });
-
-//   function addTask() {
-//     var taskContent = textarea.val();
-//     var containerToDo = $('#container-to-do');
-//     var newTask = $('<div>');
-//     newTask.addClass('new');
-//     newTask.addClass('checkbox');
-//     containerToDo.append(newTask);
-//     var label = $('<label>');
-//     var check = $('<input>');
-//     check.attr('type', 'checkbox');
-//     label.append(check);
-//     newTask.append(label);
-//     var text = $(document.createTextNode(taskContent));
-//     label.append(text);
-//     var icon = $('<i>');
-//     icon.addClass('glyphicon glyphicon-trash icon');
-//     newTask.append(icon);
-
-//     textarea.val('')
-
-//     check.click(function () {
-//       labeledTask();
-//     });
-
-//     function labeledTask() {
-//       label.toggleClass('completed');
-//     }
-//   }
-// });
