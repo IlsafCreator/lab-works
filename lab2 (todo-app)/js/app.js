b01b7178b563898f42dbd5d0d8134f8e32b1bb74
Ilsaf Khasanov <ilsaf.khasanov@mail.ru>
2019-09-30 14:28:28 +0300

    make filters

--- lab2 (todo-app)/js/app.js
+++ lab2 (todo-app)/js/app.js
@@ -6,7 +6,7 @@ window.onload = function () {
     if (txtArea.value.trim() != ""){
       addTask();
     }else{
-      alert("Please enter the text of your task.");
+      alert('Please enter the text of your task.');
     }
     
     txtArea.value = "";
@@ -20,8 +20,10 @@ window.onload = function () {
     newTask.classList.add('new', 'checkbox');
     containerToDo.append(newTask);
     var label = document.createElement('label');
+    label.classList.add('task-label');
     var check = document.createElement('input');
     check.setAttribute('type', 'checkbox');
+    check.classList.add('task-checkbox');
     label.append(check);
     newTask.append(label);
     var text = document.createTextNode(taskContent);
@@ -39,43 +41,108 @@ window.onload = function () {
 
     //delete task
     icon.onclick = function (event) {
-      event.currentTarget.closest("div.new").remove();
-      //event.currentTarget.parentElement.remove();
+      if (!event.currentTarget.closest('div.new').classList.contains('deleted')) {
+        event.currentTarget.closest('div.new').style.display = 'none';
+      }
+      event.currentTarget.closest('div.new').classList.add('deleted');
+      //event.currentTarget.closest('div.new').remove();
     };
-
+    document.getElementById('complete-all').classList.remove('active');
   }
 
   //handling Enter key
   document.querySelector('#textarea').addEventListener('keydown', function (event) {
     if (event.keyCode === 13) {
-      addTask();
+      if (txtArea.value.trim() != ""){
+        addTask();
+      }else{
+        alert('Please enter the text of your task.');
+      }
+      
+      txtArea.value = "";
     }
   })
+
+  
+
   // complete all handler 
   var completeAll = document.getElementById('complete-all');
   completeAll.onclick = function () {
-    for (let index = 0; index < document.getElementsByClassName('new checkbox').length; index++) {
-      document.getElementsByClassName('new checkbox')[index].firstChild.classList.toggle('completed');
+    var taskList = document.getElementsByClassName('new checkbox');
+
+    for (let index = 0; index < taskList.length; index++) {
+      var taskLabel = taskList[index].querySelector('.task-label')
+      var taskCheckbox = taskList[index].querySelector('.task-checkbox');
+
+      if (!taskList[index].classList.contains('deleted') && completeAll.checked) {
+        taskLabel.classList.add('completed');
+        taskCheckbox.checked = true;
+        //console.log(1);
+      } else {
+        taskLabel.classList.remove('completed');
+        taskCheckbox.checked = false;
+        //console.log(0);
+      }
+
+      taskCheckbox.onclick = function (event) {
+        completeAll.checked = false;
+
+        if (event.target.closest('.task-label').classList.contains('completed')) {
+          event.target.closest('.task-label').classList.remove('completed');
+        } else {
+          event.target.closest('.task-label').classList.add('completed');
+        }
+      }
     }
   };
   
-
-
-  // TODO: filter tasks
-  /*
+    //show all filter
     var filterShowAll = document.getElementById('filter-show-all');
     filterShowAll.onclick = function () {
-      console.log('show all');
+      var taskList = document.getElementsByClassName('new checkbox');
+      for (let index = 0; index < taskList.length; index++) {
+        if (!taskList[index].classList.contains('deleted')){
+          taskList[index].style.display = 'block';
+        }else{
+          taskList[index].style.display = 'none';
+        }
+      }
+     // console.log('show all');
     };
 
+    //show completed filter
     var filterShowCompleted = document.getElementById('filter-show-completed');
     filterShowCompleted.onclick = function () {
-      console.log('show completed');
+      var taskList = document.getElementsByClassName('new checkbox');
+      for (let index = 0; index < taskList.length; index++) {
+        if(!taskList[index].classList.contains('deleted')&&taskList[index].classList.contains('completed')){
+          taskList[index].style.display = 'block';
+        }else{
+          taskList[index].style.display = 'none';
+        }
+      }
+      //console.log('show completed');
     };
 
-    var filterShowCompleted = document.getElementById('filter-show-removed');
+    //show removed filter
+    var filterShowRemoved = document.getElementById('filter-show-removed');
     filterShowRemoved.onclick = function () {
-      console.log('show removed');
+      var taskList = document.getElementsByClassName('new checkbox');
+
+      for (let index = 0; index < taskList.length; index++) {
+        if (taskList[index].classList.contains('deleted')){
+          taskList[index].style.display = 'block';
+        }else{
+          taskList[index].style.display = 'none';
+        }
+      }
+      //console.log('show removed');
     };
-  */
+
+    //show active filter
+    var filterShowActive = document.getElementById('filter-show-active');
+    filterShowActive.onclick = function () {
+      console.log('show active');
+    };
+  
 }
