c789e62aa7cc5e593e3886682cf7068c2e916921
Ilsaf Khasanov <ilsaf.khasanov@mail.ru>
2019-10-29 10:17:57 +0300

    add positive reaction

--- lab5(js-objects&functions)/Task1/cat.js
+++ lab5(js-objects&functions)/Task1/cat.js
@@ -1,14 +1,18 @@
 var catVasiliy = {
-  name: 'Василий',
+  name: 'Василий II',
   birthday: new Date(),
   listOfFears: ['vacuum cleaner', 'beep'],
+  listOfPositive: ['ksksks'],
   meow: function () {
     console.log('Meow!');
   },
   reaction: function (feeling) {
     if (this.listOfFears.indexOf(feeling) > -1) {
       this.meow();
-      console.log('run from here!!!!')
+      console.log('run from here!!!!');
+    }
+    if (this.listOfPositive.indexOf(feeling) > -1) {
+      console.log('Hmm? Food?! I`m already running.');
     }
   },
 };
