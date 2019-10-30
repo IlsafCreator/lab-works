48a5a5aa18a8f0c3e16568b9ed152e139595b0b9
Ilsaf Khasanov <ilsaf.khasanov@mail.ru>
2019-10-30 11:53:23 +0300

    complete task-3

--- lab5(js-objects&functions)/Task3/pulloutArray.js
+++ lab5(js-objects&functions)/Task3/pulloutArray.js
@@ -1,23 +1,27 @@
 function pulloutArray(array) {
-  var resultArray = [];
-  for (let index = 0; index < array.length; index++) {
-    if (typeof array[index] === 'object') {
-      for (var j = 0; j < array[index].length; j++) {
-        resultArray.push(array[index][j]);
+  if (array !== undefined) {
+    var resultArray = [];
+    for (let index = 0; index < array.length; index++) {
+      if (Array.isArray(array[index])) {
+        for (let j = 0; j < array[index].length; j++) {
+          if ((array[index][j] !== null) && (Number.isNaN(array[index][j]) === false) && (typeof (array[index][j]) !== 'string')) {
+            resultArray.push(array[index][j]);
+          }
+        }
+      } else if ((array[index] !== null) && (Number.isNaN(array[index]) === false) && (typeof (array[index]) !== 'string')) {
+        resultArray.push(array[index]);
       }
-    } else {
-      resultArray.push(array[index]);
     }
+    return (resultArray);
+  } else {
+    return ("Функция должна принимать в качестве параметра 'Array'");
   }
-  console.log(resultArray);
 }
 
-pulloutArray([1, 2, 3]); // return [1, 2, 3]
-pulloutArray([]); // return []
-pulloutArray([1, [2, 3, 4], 5]); // return [1, 2, 3, 4, 5]
-pulloutArray([1, [2, 3, 4], 5, [1]]); // return [1, 2, 3, 4, 5, 1]
-// pulloutArray([1, [1], null, NaN]); // return [1, 1]
-// pulloutArray([1, [1], null, NaN, ['test']]); // return [1, 1]
-
-
-
+console.log(pulloutArray([1, 2, 3])); // return [1, 2, 3]
+console.log(pulloutArray([])); // return []
+console.log(pulloutArray());
+console.log(pulloutArray([1, [2, 3, 4], 5])); // return [1, 2, 3, 4, 5]
+console.log(pulloutArray([1, [2, 3, 4], 5, [1]])); // return [1, 2, 3, 4, 5, 1]
+console.log(pulloutArray([1, [3, 4], null, NaN, 'test'])); // return [1, 1]
+console.log(pulloutArray([1, [1, null, 2, 'test'], 'qwerty', null, 2, NaN, ['test', null, NaN, 'text']])); // 
