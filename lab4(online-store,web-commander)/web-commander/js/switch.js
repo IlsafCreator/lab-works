d0a916b45a853da75b01b05760248debd8ec945c
Ilsaf Khasanov <ilsaf.khasanov@mail.ru>
2019-11-05 12:00:45 +0300

    rewrite script

--- lab4(online-store,web-commander)/web-commander/js/switch.js
+++ lab4(online-store,web-commander)/web-commander/js/switch.js
@@ -1,50 +1,78 @@
+let webCommanderUI = {
+  currentIndex: 0,
+  currentCatalog: document.querySelector('.catalog.left-catalog'),
+  currentPageElements: document.querySelector('.catalog.left-catalog').children,
+  quantity: 0,
+
+  moveSelection: function () {
+    for (let i = 0; i < this.currentPageElements.length; i++) {
+      if (i == this.currentIndex) {
+        this.currentPageElements[i].classList.add('selected');
+      } else {
+        this.currentPageElements[i].classList.remove('selected');
+      }
+    }
+  },
+
+  clearSelection: function () {
+    for (let i = 0; i < this.currentPageElements.length; i++) {
+      this.currentPageElements[i].classList.remove('selected');
+    }
+  },
+
+  create: function () {
+    this.quantity = this.currentPageElements.length;
+
+    this.moveSelection();
+    document.addEventListener('keydown', function (event) {
+      //key down
+      if (event.keyCode === 40) {
+        if (this.currentIndex < this.quantity - 1) {
+          this.currentIndex += 1;
+          this.moveSelection();
+        }
+      }
+
+      // key up
+      if (event.keyCode === 38) {
+        if (this.currentIndex > 0) {
+          this.currentIndex -= 1;
+          this.moveSelection();
+        }
+      }
+
+      // key right
+      if (event.keyCode === 39) {
+        this.clearSelection();
+        this.currentIndex = 0;
+        this.currentCatalog = document.querySelector('.catalog.right-catalog');
+        if (!this.currentCatalog.classList.contains('current-catalog')) {
+          this.currentCatalog.classList.add('current-catalog');
+          document.querySelector('.catalog.left-catalog').classList.remove('current-catalog');
+        }
+        this.currentPageElements = document.querySelector('.catalog.right-catalog').children;
+        this.quantity = this.currentPageElements.length;
+        this.moveSelection();
+      }
+
+      // key left
+      if (event.keyCode === 37) {
+        this.clearSelection();
+        this.currentIndex = 0;
+        this.currentCatalog = document.querySelector('.catalog.left-catalog');
+        if (!this.currentCatalog.classList.contains('current-catalog')) {
+          this.currentCatalog.classList.add('current-catalog');
+          document.querySelector('.catalog.right-catalog').classList.remove('current-catalog');
+        }
+        this.currentPageElements = document.querySelector('.catalog.left-catalog').children;
+        this.quantity = this.currentPageElements.length;
+        this.moveSelection();
+        // console.log('Left tab active');
+      }
+    }.bind(this));
+  }
+};
+
 window.onload = function () {
-
-  let currentPage = document.querySelector('.catalog.left-catalog');
-
-  currentIndex = -1;
-
-  document.addEventListener('keydown', function (eventParent) {
-
-    let currentPageElements = currentPage.children;
-
-    //key down
-    if (eventParent.keyCode === 40 && currentIndex < currentPageElements.length && (currentIndex < currentPageElements.length - 1)) {
-      if (currentIndex + 1 < currentPageElements.length) {
-        currentIndex += 1;
-      }
-
-      // console.log('Текущий индекс', currentIndex);
-
-      currentPageElements[currentIndex].classList.toggle('selected');
-      if (currentIndex > 0) {
-        currentPageElements[currentIndex - 1].classList.toggle('selected');
-      }
-    }
-    //key up
-    if (eventParent.keyCode === 38 && currentIndex > 0) {
-      currentIndex -= 1;
-      currentPageElements[currentIndex + 1].classList.toggle('selected');
-      currentPageElements[currentIndex].classList.toggle('selected');
-    }
-
-    //key right
-    if (event.keyCode === 39) {
-      currentPageElements[currentIndex].classList.toggle('selected');
-      currentPage = document.querySelector('.catalog.right-catalog');
-      currentPage.classList.toggle("current-catalog");
-      document.querySelector('.catalog.left-catalog').classList.toggle("current-catalog");
-      // console.log("нажата клавиша вправо");
-      currentIndex = -1;
-    }
-    //key left
-    if (event.keyCode === 37) {
-      currentPageElements[currentIndex].classList.toggle('selected');
-      currentPage = document.querySelector('.catalog.left-catalog');
-      currentPage.classList.toggle("current-catalog");
-      document.querySelector('.catalog.right-catalog').classList.toggle("current-catalog");
-      // console.log("нажата клавиша влево");
-      currentIndex = -1;
-    }
-  });
+  webCommanderUI.create();
 }
