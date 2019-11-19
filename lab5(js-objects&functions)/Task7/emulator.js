32f5624ab153a1ba5c090effd8d515b5d26e4d19
Ilsaf Khasanov <ilsaf.khasanov@mail.ru>
2019-11-19 15:02:36 +0300

    initial commit

--- lab5(js-objects&functions)/Task7/emulator.js
+++ lab5(js-objects&functions)/Task7/emulator.js
@@ -2,13 +2,27 @@ var soldier1 = {
   name: 'John',
   health: 100,
   currentCoordinates: { x: 0, y: 0 },
-  shot: function (x, y) {
-    console.log(x, y);
+  shot: function () {
+    // console.log();
+    if (this.health > 0) {
+      var shotX = Math.floor(Math.random() * 5);
+      var shotY = Math.floor(Math.random() * 5);
+      console.log(this.name, 'выстрелил в координаты [' + shotX + ';' + shotY + ']');
+      return [shotX, shotY];
+    }
   },
   spawn: function () {
     this.currentCoordinates.x = Math.floor(Math.random() * 5);
     this.currentCoordinates.y = Math.floor(Math.random() * 5);
     console.log(this.name + ' spawned at ' + this.currentCoordinates.x + ' ; ' + this.currentCoordinates.y);
+  },
+  checkDamage: function (x, y) {
+    if (x === this.currentCoordinates.x && y === this.currentCoordinates.y) {
+      this.health -= 25;
+      return ('По ' + this.name + '`у попали ');
+    } else {
+      return ('По ' + this.name + '`у промазали ');
+    }
   }
 };
 
@@ -16,13 +30,27 @@ var soldier2 = {
   name: 'Gook',
   health: 100,
   currentCoordinates: { x: 0, y: 0 },
-  shot: function (x, y) {
-    console.log(x, y);
+  shot: function () {
+    // console.log(x, y);
+    if (this.health > 0) {
+      var shotX = Math.floor(Math.random() * 5);
+      var shotY = Math.floor(Math.random() * 5);
+      console.log(this.name, 'выстрелил в координаты [' + shotX + ';' + shotY + ']');
+      return [shotX, shotY];
+    }
   },
   spawn: function () {
     this.currentCoordinates.x = Math.floor(Math.random() * 5);
     this.currentCoordinates.y = Math.floor(Math.random() * 5);
     console.log(this.name + ' spawned at ' + this.currentCoordinates.x + ' ; ' + this.currentCoordinates.y);
+  },
+  checkDamage: function (x, y) {
+    if (x === this.currentCoordinates.x && y === this.currentCoordinates.y) {
+      this.health -= 25;
+      return ('По ' + this.name + '`у попали ');
+    } else {
+      return ('По ' + this.name + '`у промазали ');
+    }
   }
 };
 soldier1.spawn();
@@ -30,25 +58,25 @@ soldier2.spawn();
 
 setInterval(function () {
   if (soldier1.health > 0 && soldier2.health > 0) {
-    var shotX = Math.floor(Math.random() * 5);
-    var shotY = Math.floor(Math.random() * 5);
-    soldier1.shot(shotX, shotY);
-    if (shotX === soldier2.currentCoordinates.x && shotY === soldier2.currentCoordinates.y) {
-      console.log(soldier1.name, 'попал по: ', soldier2.name);
-      soldier2.health -= 25;
+    if (soldier1.health > 0) {
+      arr = soldier1.shot();
+      console.log(soldier2.checkDamage(arr[0], arr[1]));
     }
-    var shotX = Math.floor(Math.random() * 5);
-    var shotY = Math.floor(Math.random() * 5);
-    soldier2.shot(shotX, shotY);
-    if (shotX === soldier1.currentCoordinates.x && shotY === soldier1.currentCoordinates.y) {
-      console.log(soldier2.name, 'попал по: ', soldier1.name);
-      soldier1.health -= 25;
+    // arr = soldier1.shot();
+    // soldier2.checkDamage(arr[0], arr = [1]);
+    if (soldier2.health > 0) {
+      arr1 = soldier2.shot();
+      console.log(soldier1.checkDamage(arr1[0], arr1[1]));
     }
+    // arr1 = soldier2.shot();
+    // soldier1.checkDamage(arr1[0], arr1 = [1]);
 
     if (soldier1.health <= 0) {
-      console.log(soldier2.name, 'выиграл!');
+      console.log(soldier1.name, 'проиграл! :с');
+      console.log(soldier2.name, 'выиграл! :3');
     } else if (soldier2.health <= 0) {
-      console.log(soldier1.name, 'выиграл!');
+      console.log(soldier1.name, 'проиграл! :с');
+      console.log(soldier2.name, 'выиграл! :3');
     }
   }
 }, 100);
