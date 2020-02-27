96119265d916a738125ee91d73361fe00771ff59
Ilsaf Khasanov <ilsaf.khasanov@mail.ru>
2020-02-27 20:25:17 +0300

    completed player function

--- lab6/src/task-2/js/player.js
+++ lab6/src/task-2/js/player.js
@@ -1,62 +1,97 @@
 'use strict';
-function Song(songName, songLength) {
-  this.songName = songName;
-  this.songLength = songLength;
+
+class Song {
+  constructor(songName, songLength) {
+    this.songName = songName;
+    this.songLength = songLength;
+  }
+  getSongName() {
+    return this.songName;
+  }
 }
 
-function PlayerList(song) {
-  this.songs = [song];
-
-
-  //перенсти переключение треков из player'а в этот блок
+class PlayerList {
+  constructor(songs) {
+    this.songs = songs || [];
+    this.currentTrackIndex = 0;
+    if (songs && songs.length !== 0) {
+      this.currentTrackName = songs[this.currentTrackIndex].getSongName();
+    }
+  }
+  next() {
+    if (this.songs && this.songs.length !== 0) {
+      this.currentTrackIndex += 1;
+      if (this.currentTrackIndex > this.songs.length - 1) {
+        this.currentTrackIndex = 0;
+      }
+      this.currentTrackName = this.songs[this.currentTrackIndex].getSongName();
+    }
+  }
+  prev() {
+    if (this.songs && this.songs.length !== 0) {
+      this.currentTrackIndex -= 1;
+      if (this.currentTrackIndex < 0) {
+        this.currentTrackIndex = this.songs.length - 1;
+      }
+      this.currentTrackName = this.songs[this.currentTrackIndex].getSongName();
+    }
+  }
+  getCurrentTrack() {
+    return this.currentTrackName;
+  }
 }
 
-function Player(playerList) {
-  this.playerList = playerList;
-  this.currentTrack = playerList.songs[0][0].songName;
-  this.status = 'stop';
-  let index = 0;
-
-  this.display = function () {
-    return 'Track: ' + this.currentTrack + '; Status: ' + this.status;
-  };
-
-  this.play = function () {
-    this.status = 'play';
-  };
-
-  this.stop = function () {
-    this.status = 'play';
-  };
-
-  this.pause = function () {
-    this.status = 'pause';
-  };
-
-  this.next = function () {
-    if (index != playerList.songs[0].length - 1) {
-      index++;
-      this.currentTrack = playerList.songs[0][index].songName;
+class Player {
+  constructor(playerList) {
+    this.playerList = new PlayerList(playerList);
+    this.status = 'stop';
+  }
+  display() {
+    if (this.playerList.songs.length > 0) {
+      return 'Track: ' + this.playerList.getCurrentTrack() + '; Status: ' + this.status;
+    } else {
+      return 'Player list is empty';
     }
-    else {
-      index = 0;
-      this.currentTrack = playerList.songs[0][index].songName;
+  }
+  play() {
+    if (this.playerList.songs.length > 0) {
+      this.status = 'play';
     }
-  };
-
-  this.prev = function () {
-    if (index != 0) {
-      index--;
-      this.currentTrack = playerList.songs[0][index].songName;
+  }
+  pause() {
+    if (this.playerList.songs.length > 0) {
+      this.status = 'pause';
     }
-    else {
-      index = playerList.songs[0].length - 1;
-      this.currentTrack = playerList.songs[0][index].songName;
+  }
+  stop() {
+    if (this.playerList.songs.length > 0) {
+      this.status = 'stop';
     }
-  };
+  }
+  next() {
+    this.playerList.next();
+  }
+  prev() {
+    this.playerList.prev();
+  }
 }
-
-var playerList = new PlayerList([new Song('Children of cyberpunk', 209),
+const player = new Player([new Song('Children of cyberpunk', 209),
 new Song('Beat it', 211), new Song('Country road', 240),
 new Song('Venger', 310), new Song('Чёрное солнце', 289)]);
-var player = new Player(playerList);
+
+console.log(player.next());
+console.log(player.next());
+console.log(player.display());
+console.log(player.prev());
+console.log(player.display());
+console.log(player.play());
+console.log(player.display());
+console.log(player.pause());
+console.log(player.display());
+
+const player2 = new Player([]);
+console.log(player2.display());
+console.log(player2.next());
+console.log(player2.display());
+console.log(player2.prev());
+console.log(player2.display());
